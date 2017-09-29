0968ac7de13376a8e42b97251870b5d69d10c409
Tung Vu <tungtd95@gmail.com>
2017-09-29 15:45:36 +0700

    add some js

---
+++ server/src/java/db/DBHelper.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Data;
+import model.Device;
+import model.User;
+
+/**
+ *
+ * @author tung
+ */
+public class DBHelper {
+    Connection connection;
+    User mUser;
+    Device mDevice;
+    Data mData;
+    
+    /*
+    * create connection to database
+    */
+    public DBHelper() {
+        try { 
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:"
+                    + "3307/thong-tin-di-dong?useSSL=false", "root", "Javafirst"); 
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    /*
+    *status of checkUser method
+    *   -1: not sign in
+    *    0: signed in but password does not match
+    *    1: password matched and checkUser success
+    *    2: database connection error
+    */
+    public int checkUser() {
+        int status;
+        Statement statement;
+        ResultSet resultSet;
+        
+        String query = "SELECT * FROM `thong-tin-di-dong`.user "
+                + "where user.user_name='"+mUser.getUserName()+"';";
+        
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            if (resultSet.next()) {
+                String password = resultSet.getString("user_password");
+                if (mUser.getPassword().equals(password)) {
+                    status = 1;
+                } else {
+                    status = 0;
+                }
+            } else {
+                status = -1;
+            }
+        } catch (SQLException ex) {
+            status = 2;
+            Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return status;
+    }
+    
+    /*
+    *status of device:
+    *    0: not add yet
+    *    1: existed
+    */
+    public int checkDevice() {
+        int status = -1;
+        Statement statement;
+        ResultSet resultSet;
+        String query = "SELECT * FROM `thong-tin-di-dong`.device "
+                + "where device.device_imei='"+mDevice.getDeviceIMEI()+"';";
+        
+        return status;
+    }
+
+    public User getmUser() {
+        return mUser;
+    }
+
+    public void setmUser(User mUser) {
+        this.mUser = mUser;
+    }
+
+    public Device getmDevice() {
+        return mDevice;
+    }
+
+    public void setmDevice(Device mDevice) {
+        this.mDevice = mDevice;
+    }
+
+    public Data getmData() {
+        return mData;
+    }
+
+    public void setmData(Data mData) {
+        this.mData = mData;
+    }
+    
+    public void finish() {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ server/src/java/main/add.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import db.DBHelper;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Data;
+import model.Device;
+import model.User;
+
+/**
+ *
+ * @author tung
+ */
+@WebServlet(name = "add", urlPatterns = {"/add"})
+public class add extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        DBHelper dbHelper = new DBHelper();
+        int user_status;
+        int data_status = -1;
+        
+        String userName = request.getParameter("userName");
+        String userPassword = request.getParameter("userPassword");
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(userPassword);
+        dbHelper.setmUser(user);
+        
+        String deviceIMEI = request.getParameter("deviceIMEI");
+        String deviceName = request.getParameter("deviceName");
+        Device device = new Device();
+        device.setDeviceIMEI(deviceIMEI);
+        device.setDeviceName(deviceName);
+        dbHelper.setmDevice(device);
+        
+        float humidity = Float.valueOf(request.getParameter("humid"));
+        float temperature = Float.valueOf(request.getParameter("temp"));
+        String time = request.getParameter("time");
+        String coordinate = request.getParameter("coord");
+        float pin = Float.valueOf(request.getParameter("pin"));
+        float pressure = Float.valueOf(request.getParameter("pressure"));
+        Data data = new Data();
+        data.setCoordinate(coordinate)
+                .setHumidity(humidity)
+                .setPin(pin)
+                .setPressure(pressure)
+                .setTemperature(temperature)
+                .setTime(time);
+        dbHelper.setmData(data);
+        
+        user_status = dbHelper.checkUser();
+        
+        PrintWriter echo;
+        try {
+            echo = response.getWriter();
+            String content = "{\"user status\":" + user_status+","
+                    + "\"data status\":"+data_status+"}";
+            echo.println(content);
+        } catch (IOException ex) {
+            Logger.getLogger(add.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
